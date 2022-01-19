81333236332ee8bd725bd344d825e72574e48c80
tomanski <toman@posteo.eu>
2022-01-19 11:43:00 +0200

    Update Example.gd

--- Example.gd
+++ Example.gd
@@ -3,10 +3,18 @@ extends Node
 
 onready var input:GamepadInput = get_node("GamepadInput")
 
+func _ready() -> void:
+	input.ID = 0 #The gamepad to read input from
+	input.deadzone = 0.2 #Deadzone for the thumbsticks
+
+
 func _process(delta) -> void:
 	if input.left_stick:
-		#DO STUFF:
-    pass
+		#Returns a normalized Vector2
+    		pass
+	if input.right_stick:
+		#Returns a normalized Vector2
+		pass
 
   if input.btn_a_state == input.STATE.JUST_PRESSED:
     #DO STUFF:
